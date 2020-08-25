0f10c8697adebd734136f02dc9aa01ab7d5064f0
Purvaja Gopalakrishnan <purvaja.gopalakrishnan@cesltd.com>
2020-08-25 22:01:46 +0530

    Created an angular app for products

---
+++ src/app/products/product-detail.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDetailGuard implements CanActivate {
+
+  constructor(private router: Router) { }
+
+  canActivate(activatedRouteNext: ActivatedRouteSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    const id = +activatedRouteNext.url[1].path;
+    if (isNaN(id) || id < 1) {
+      alert('Invalid product Id');
+      this.router.navigate(['/products']);
+      return false;
+    }
+    return true;
+  }
+}
